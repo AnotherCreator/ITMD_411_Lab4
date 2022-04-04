3d731cd87823e614f3471e814f0b4e0128b84f7a
Josh <jreginaldo@protonmail.com>
2022-04-04 11:55:16 -0500

    Init database related files

---
+++ src/LoanProcessing.java
@@ -0,0 +1,15 @@
+/*
+Josh Reginaldo
+ITMD-411
+
+Lab 4 - Database Implementation
+
+Use a database to store and present loan analysis information
+Driver class to deal with the database
+*/
+
+public class LoanProcessing {
+    public static void main(String[] args) {
+
+    }
+}
