f9da0fffb4adf893732909a9a245af9e3a78a92e
Josh <jreginaldo@protonmail.com>
2022-04-04 12:31:51 -0500

    Implement connection to database

--- src/DbConnect.java
+++ src/DbConnect.java
@@ -1,2 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import  java.sql.SQLException;
+
 public class DbConnect {
+    // Database URL
+    static final String DB_URL = "jdbc:mysql://www.papademas.net:3307/411labs?autoReconnect=true&useSSL=false";
+    // Database Credentials
+    static final String USER = "db411", PASS = "411";
+
+    public Connection connect() throws SQLException {
+        return DriverManager.getConnection(DB_URL, USER, PASS);
+    }
 }
