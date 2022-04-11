581260cf3199b7740ecdfa982d7db8179cf2b76e
Josh <jreginaldo@protonmail.com>
2022-04-11 11:53:56 -0500

    Put database handling into a try/catch block

--- src/LoanProcessing.java
+++ src/LoanProcessing.java
@@ -12,23 +12,30 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class LoanProcessing extends BankRecords {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         LoanProcessing records = new LoanProcessing();
         records.readClientData();
         records.processClientData();
 
         // Database handling
-        Dao DBAccess = new Dao();
-        DBAccess.createTable();
-        DBAccess.insertRecords(bankEntry);
-        ResultSet rs = DBAccess.retrieveRecords();
+        try {
+            Dao DBAccess = new Dao();
+            DBAccess.createTable();
+            DBAccess.insertRecords(bankEntry);
+            ResultSet rs = DBAccess.retrieveRecords();
 
-        // Iterate and print result set
-        System.out.println("ID:\t\tINCOME:\t\tPEP:");
-        while (rs.next()) {
+            // Iterate and print result set
+            System.out.println("ID:\t\t\tINCOME:\t\t\tPEP:");
+            while (rs.next()) {
+                String ID = rs.getString("id");
+                double income = rs.getDouble("income");
+                String pep = rs.getString("pep");
 
+                System.out.printf("\n%s \t%.2f \t\t%s", ID, income, pep);
+            }
+            rs.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
         }
-
-        rs.close();
     }
 }
