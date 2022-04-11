7614c180b5fbed2fa3db949cd1000b0398237bc8
Josh <jreginaldo@protonmail.com>
2022-04-11 11:06:05 -0500

    Add database handling to main driver file

--- src/LoanProcessing.java
+++ src/LoanProcessing.java
@@ -8,8 +8,27 @@ Use a database to store and present loan analysis information
 Driver class to deal with the database
 */
 
-public class LoanProcessing {
-    public static void main(String[] args) {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+public class LoanProcessing extends BankRecords {
+    public static void main(String[] args) throws SQLException {
+        LoanProcessing records = new LoanProcessing();
+        records.readClientData();
+        records.processClientData();
+
+        // Database handling
+        Dao DBAccess = new Dao();
+        DBAccess.createTable();
+        DBAccess.insertRecords(bankEntry);
+        ResultSet rs = DBAccess.retrieveRecords();
+
+        // Iterate and print result set
+        System.out.println("ID:\t\tINCOME:\t\tPEP:");
+        while (rs.next()) {
+
+        }
+
+        rs.close();
     }
 }
